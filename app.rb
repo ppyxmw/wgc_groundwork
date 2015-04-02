2012a21701ac83bbb295dee85ec61778d87b0dc6
Michael Worthingon <ppyxmw@gmail.com>
2015-04-02 13:07:53 +0200

    this is a commit

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require './lib/profile'
 
-set :application_id, 'EDIT_ME'
-set :secret, 'EDIT_ME'
+set :application_id, 'f3883d377740564569736af4d3c744d7d360a4537765c894b4023dfa980046fc'
+set :secret, 'c2117b9b9dd2bdfb3d2deaf2dc3f601f9abc351f6431fc9eeca96c2ecc39313c'
 set :redirect_uri, 'http://localhost:4567/callback'
 set :site_url, 'http://wegotcoders.com'
 set :session_secret, 'secret'
